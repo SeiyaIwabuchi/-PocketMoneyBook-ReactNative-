012f5f27ea23042996b34bacaf0d5a065a127ade
SeiyaIwabuchi <0123ook@gmail.com>
2020-12-08 10:36:27 +0900

    #2 修正

--- Validator.ts
+++ Validator.ts
@@ -11,14 +11,20 @@ export class ValidationResult{
 }
 
 export function validation(data:BalanceData){
+    let isValid  = true;
     let errorText = "";
     if(isNaN(parseInt(data.price))){
         errorText += "金額が不正です。\n";
+        isValid = false;
     }
     if(data.content === ""){
         errorText += "事柄を入力してください。\n"
+        isValid = false;
     }
     let tDate = data.date.split("/");
-    if(isNaN(parseInt(tDate[0])) || isNaN(parseInt(tDate[1])) || 1 > parseInt(tDate[0]) || parseInt(tDate[1]) > 12) errorText += "日付が不正です。\n";
-    return new ValidationResult(false,errorText);
+    if(isNaN(parseInt(tDate[0])) || isNaN(parseInt(tDate[1])) || 1 > parseInt(tDate[0]) || parseInt(tDate[1]) > 12){
+        errorText += "日付が不正です。\n";
+        isValid = false;
+    }
+    return new ValidationResult(isValid,errorText);
 }
