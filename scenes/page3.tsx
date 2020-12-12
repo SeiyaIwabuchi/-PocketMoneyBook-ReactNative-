137f7b47a55a50485abbbc130810cb36a4c8bbe2
SeiyaIwabuchi <0123ook@gmail.com>
2020-12-12 22:56:10 +0900

    インポートできないバグ修正

--- scenes/page3.tsx
+++ scenes/page3.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Picker, Text, Dimensions,Clipboard } from "react-native";
+import { View, Picker, Text, Dimensions, Clipboard } from "react-native";
 import { Button, Header, normalize } from "react-native-elements";
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from "@react-native-community/async-storage";
@@ -13,12 +13,12 @@ export default function page3() {
 	const [firstDayOfTheWeek, setFirstDayOfTheWeek] = useState(-1);
 	const date = new Date();
 	const lastDateOfTheMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-	const [balanceDataList,setBalanceDataList] = useState<BalanceData[]>([]);
-	const [visible,setVisible] = useState(false);
-	const [snackbarText,setSnackbarText] = useState("");
-	const [deleteTableCount,setDeleteTableCount] = useState(0);
-	const [snackbarButtonText,setSnackbarButtonText] = useState("");
-	const [isUpdateAvailable,setIsUpdateAvailable] = useState(false);
+	const [balanceDataList, setBalanceDataList] = useState<BalanceData[]>([]);
+	const [visible, setVisible] = useState(false);
+	const [snackbarText, setSnackbarText] = useState("");
+	const [deleteTableCount, setDeleteTableCount] = useState(0);
+	const [snackbarButtonText, setSnackbarButtonText] = useState("");
+	const [isUpdateAvailable, setIsUpdateAvailable] = useState(false);
 	useFocusEffect(
 		React.useCallback(() => {
 			AsyncStorage.getItem("firstDateOfTheMonth")
@@ -33,17 +33,18 @@ export default function page3() {
 						setFirstDayOfTheWeek(parseInt(date));
 					}
 				});
-				Db.select(setBalanceDataList,(balalist)=>{});
-				( async ()=> {try {
-							const update = await Updates.checkForUpdateAsync();
-							setIsUpdateAvailable(update.isAvailable);
-							} catch (e) {
-							// handle or log error
-								console.log("confirm updates error");
-								console.log(e);
-							}
-						}
-				)();
+			Db.select(setBalanceDataList, (balalist) => { });
+			(async () => {
+				try {
+					const update = await Updates.checkForUpdateAsync();
+					setIsUpdateAvailable(update.isAvailable);
+				} catch (e) {
+					// handle or log error
+					console.log("confirm updates error");
+					console.log(e);
+				}
+			}
+			)();
 		}, [])
 	);
 	return (
@@ -51,14 +52,14 @@ export default function page3() {
 			<Header
 				placement={"left"}
 				centerComponent={{ text: "お貧乏様", style: { fontSize: 25 } }}
-				containerStyle={{shadowOpacity:1,shadowRadius:0,shadowOffset:{width:0,height:1},shadowColor:"#ccc",elevation:2}}
+				containerStyle={{ shadowOpacity: 1, shadowRadius: 0, shadowOffset: { width: 0, height: 1 }, shadowColor: "#ccc", elevation: 2 }}
 			/>
 			<View style={{ alignItems: "center", justifyContent: "center", height: "100%", padding: "1%" }}>
 				<View style={{ height: "100%", width: "100%", padding: "10%" }}>
-					<View style={{flexDirection:"row",height:"10%",width:"100%",justifyContent:"space-between",marginBottom:"3%",alignItems:"center"}}>
-						<Text style={{fontSize:normalize(15)}}>{"月始め日"}</Text>
+					<View style={{ flexDirection: "row", height: "10%", width: "100%", justifyContent: "space-between", marginBottom: "3%", alignItems: "center" }}>
+						<Text style={{ fontSize: normalize(15) }}>{"月始め日"}</Text>
 						<Picker style={{
-							width:"30%"
+							width: "30%"
 						}}
 							selectedValue={firstDateOfTheMonth}
 							onValueChange={(item) => {
@@ -76,10 +77,10 @@ export default function page3() {
 							})()}
 						</Picker>
 					</View>
-					<View  style={{flexDirection:"row",height:"10%",width:"100%",justifyContent:"space-between",marginBottom:"3%",alignItems:"center"}}>
-						<Text style={{fontSize:normalize(15)}}>{"週始まり曜日"}</Text>
+					<View style={{ flexDirection: "row", height: "10%", width: "100%", justifyContent: "space-between", marginBottom: "3%", alignItems: "center" }}>
+						<Text style={{ fontSize: normalize(15) }}>{"週始まり曜日"}</Text>
 						<Picker style={{
-							width:"30%"
+							width: "30%"
 						}}
 							selectedValue={firstDayOfTheWeek}
 							onValueChange={(item) => {
@@ -98,53 +99,83 @@ export default function page3() {
 							})()}
 						</Picker>
 					</View>
-						<Button title={"テーブル削除"} onPress={()=>{
-							if(deleteTableCount === 3){
-								setDeleteTableCount(0);
-								Db.deleteTable();
-								setSnackbarText("テーブルを削除しました。");
-								setSnackbarButtonText("OK");
-								setVisible(true);
-							}else{
-								setDeleteTableCount(deleteTableCount+1);
-								setSnackbarText(`あと${3-deleteTableCount}でテーブルを削除します。`);
-								setSnackbarButtonText("CANCEL");
-								setVisible(true);
-							}
-							}}/>
-						<Button title={"テーブル作成"} onPress={()=>{Db.createTable()}}/>
-						<Button title={"テーブル確認"} onPress={()=>{Db.select(()=>{},(balaList)=>{console.log(balaList)})}}/>
-						<Button title={"DB2JSON"} onPress={()=>{
-							console.log(JSON.stringify(balanceDataList,null,"\t"));
-							Clipboard.setString(JSON.stringify(balanceDataList,undefined,"\t"));
-						}}/>
-						<Button title={"Json読込とDB更新"} onPress={()=>{
-							Clipboard.getString()
-							.then((data)=>{
-								let newBalaData:BalanceData[] = JSON.parse(data);
-								newBalaData.forEach((baladata)=>{
-									Db.insertToDb(baladata);
-								});
-							})
-						}} />
-						<Text>{`New OTA update is ${isUpdateAvailable}`}</Text>
-						<Snackbar
-							visible={visible}
-							onDismiss={() => { setVisible(false) }}
-							action={{
-								label: snackbarButtonText,
-								onPress: ()=>{
-									setDeleteTableCount(0);
-									if(snackbarButtonText === "CANCEL"){
-										setSnackbarText("カウントをリセットしました。");
-										setSnackbarButtonText("OK");
-									}else{
-										setVisible(false);
-									}
+					<Button title={"テーブル削除"} onPress={() => {
+						if (deleteTableCount === 3) {
+							setDeleteTableCount(0);
+							Db.deleteTable();
+							setSnackbarText("テーブルを削除しました。");
+							setSnackbarButtonText("OK");
+							setVisible(true);
+						} else {
+							setDeleteTableCount(deleteTableCount + 1);
+							setSnackbarText(`あと${3 - deleteTableCount}でテーブルを削除します。`);
+							setSnackbarButtonText("CANCEL");
+							setVisible(true);
+						}
+					}} />
+					<Button title={"テーブル作成"} onPress={() => { Db.createTable() }} />
+					<Button title={"テーブル確認"} onPress={() => { Db.select(() => { }, (balaList) => { console.log(balaList) }) }} />
+					<Button title={"DB2JSON"} onPress={() => {
+						let tBalaList:{date:Date,kind:string,content:string,price:string,id:number}[] = [];
+						balanceDataList.forEach((arow)=>{
+							tBalaList.push(
+								{
+									date:arow.date,
+									kind:arow.kind,
+									content:arow.content,
+									price:arow.price,
+									id:arow.id
 								}
-							}}
-							style={{width:"100%"}}
-						>{snackbarText}</Snackbar>
+								);
+						});
+						console.log(JSON.stringify(tBalaList, null, "\t"));
+						Clipboard.setString(JSON.stringify(tBalaList, undefined, "\t"));
+					}} />
+					<Button title={"Json読込とDB更新"} onPress={() => {
+						if (deleteTableCount === 3) {
+							Clipboard.getString()
+								.then((data) => {
+									let newBalaData: {date:number,kind:string,content:string,price:string,id:number}[] = JSON.parse(data);
+
+									newBalaData.forEach((baladata) => {
+										Db.insertToDb(new BalanceData(
+											new Date(baladata.date),
+											baladata.kind,
+											baladata.content,
+											baladata.price,
+											baladata.id
+										));
+									});
+									setDeleteTableCount(0);
+									setSnackbarText("テーブルを更新しました。");
+									setSnackbarButtonText("OK");
+									setVisible(true);
+								});
+						} else {
+							setDeleteTableCount(deleteTableCount + 1);
+							setSnackbarText(`あと${3 - deleteTableCount}でテーブルを更新します。`);
+							setSnackbarButtonText("CANCEL");
+							setVisible(true);
+						}
+					}} />
+						<Text>{`New OTA update is ${isUpdateAvailable}`}</Text>
+					<Snackbar
+						visible={visible}
+						onDismiss={() => { setVisible(false) }}
+						action={{
+							label: snackbarButtonText,
+							onPress: () => {
+								setDeleteTableCount(0);
+								if (snackbarButtonText === "CANCEL") {
+									setSnackbarText("カウントをリセットしました。");
+									setSnackbarButtonText("OK");
+								} else {
+									setVisible(false);
+								}
+							}
+						}}
+						style={{ width: "100%" }}
+					>{snackbarText}</Snackbar>
 				</View>
 			</View>
 		</View>
